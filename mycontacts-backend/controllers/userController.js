82c0ee3b5cf00ab29b0233f018f41925a021cd96
Sophonias Demiss <sophidemiss@gmail.com>
2025-03-04 14:21:24 +0300

    added jwt for auth

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,43 +1,72 @@
-const asyncHandler = require("express-async-handler")
-const User = require("../models/userModel")
-const bcrypt = require("bcrypt")
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const getusers = asyncHandler(async (req, res) => {
-    const users = User.find()
-    res.status(200).json(users)
-})
+  const users = await User.find();
+  res.status(200).json(users);
+});
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body
-    if (!username || !email, !password) {
-        res.status(400)
-        throw new Error("All fields are required")
-    }
+  const { username, email, password } = req.body;
+  if ((!username || !email, !password)) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
-    const useralreadyexists = await User.findOne({ email })
-    if (useralreadyexists) {
-        res.status(400)
-        throw new Error("The user already exists")
-    }
+  const useralreadyexists = await User.findOne({ email });
+  if (useralreadyexists) {
+    res.status(400);
+    throw new Error("The user already exists");
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-    console.log("hashedPassword", hashedPassword)
-    const newUser = await User.create({username, email, password: hashedPassword})
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("hashedPassword", hashedPassword);
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    if (newUser) {
-        res.status(201).json({_id: newUser.id, email: newUser.email })
-    } else {
-        res.status(400)
-        throw new Error("Invalid user information")
-    }
-})
+  if (newUser) {
+    res.status(201).json({ _id: newUser.id, email: newUser.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user information");
+  }
+});
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.send("works to login")
-})
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("email and password are required");
+  }
+
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(400);
+    throw new Error("invalid email or password");
+  }
+});
 
 const currentUser = asyncHandler(async (req, res) => {
-    res.send("works to current")
-})
+  res.json(req.user)
+});
 
-module.exports = { registerUser, loginUser, currentUser, getusers }
+module.exports = { registerUser, loginUser, currentUser, getusers };
--- mycontacts-backend/routes/userroute.js
+++ mycontacts-backend/routes/userroute.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const { registerUser, loginUser, currentUser, getusers } = require("../controllers/userController")
+const validateToken = require("../middleware/validateToken")
 
+
+router.route("/").get(getusers)
 
 router.route("/register").post(registerUser)
 
 router.route("/login").post(loginUser)
 
-router.route("/current").get(currentUser)
+router.route("/current").get(validateToken, currentUser)
 
-router.route("/").get(getusers)
 
 module.exports = router
